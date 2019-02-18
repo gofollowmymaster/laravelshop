af9f13a3fdbe80df6e5bc639624c10295b3b3d9a
zh <1950796924@qq.com>
2019-02-18 16:25:44 +0800

    封装业务代码

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Policies\UserAddressPolicy;
+use App\Policies\OrderPolicy;
+use App\Models\Order;
+use App\Models\UserAddress;
+
+
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -15,7 +21,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
         UserAddress::class => UserAddressPolicy::class,
-        Order::class       => OrderPolicy::class,
+        Order::class       =>  OrderPolicy::class,
     ];
 
     /**
