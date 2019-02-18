58224e19cc10cffccbb37d6759aeafc030cce836
zh <1950796924@qq.com>
2019-02-18 16:32:55 +0800

    引入支付库

---
+++ config/pay.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zh
+ * Date: 2019/2/18
+ * Time: 16:29
+ */
+
+return [
+    'alipay' => [
+        'app_id'         => '',
+        'ali_public_key' => '',
+        'private_key'    => '',
+        'log'            => [
+            'file' => storage_path('logs/alipay.log'),
+        ],
+    ],
+
+    'wechat' => [
+        'app_id'      => '',
+        'mch_id'      => '',
+        'key'         => '',
+        'cert_client' => '',
+        'cert_key'    => '',
+        'log'         => [
+            'file' => storage_path('logs/wechat_pay.log'),
+        ],
+    ],
+];
